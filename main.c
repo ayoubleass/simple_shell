60b718a29a40e71af89a1e43f83f5c462940e61c
ayoubleass <ayoubleas@gmail.com>
2023-11-15 07:03:48 -0800

    simple shell

--- main.c
+++ main.c
@@ -17,22 +17,28 @@ int main(int argc, char **argv)
 		/*int status;*/
 		ssize_t len;
 		/*char **arguments;*/
-
-		printf("#cisfun$ ");
+		if (isatty(fileno(stdin)) == 1)
+			printf("#cisfun$ ");
 		len = getline(&lineptr, &n, stdin);
-		removenewtag(lineptr, strlen(lineptr));
+		lineptr[len - 1] = '\0';
 		/*arguments = setexecveArgs(lineptr);*/
 		if (len == EOF)
 		{
 			free(lineptr);
-			if (isatty(fileno(stdin)) == 1)
-				_putchar('\n');
+			_putchar('\n');
 			exit(EXIT_SUCCESS);
 		}
 		else
 		{
-			if(lineptr != NULL && lineptr[0] != '\0')
+			if (lineptr != NULL && lineptr[0] != '\0')
+			{
 				execute_cmd_no_arguments(lineptr, argv[0]);
+				if (isatty(fileno(stdin)) != 1)
+				{
+					free(lineptr);
+					exit(EXIT_SUCCESS);
+				}
+			}
 		}
 	}
 }
